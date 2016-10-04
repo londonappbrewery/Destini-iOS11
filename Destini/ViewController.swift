e70980693e1d20c13f5bbbeef491bd6dae1aec5f
angelabauer <angela_yu@hotmail.co.uk>
2016-10-04 14:40:54 +0100

    updated to Xcode 8

--- Destini/ViewController.swift
+++ Destini/ViewController.swift
@@ -7,23 +7,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    // Our strings
-    let T1_Story = "Your car has blown a tire on a winding road in the middle of nowhere with no cell phone reception. You decide to hitchhike. A rusty pickup truck rumbles to a stop next to you. A man with a wide brimmed hat with soulless eyes opens the passenger door for you and asks: \"Need a ride, boy?\"."
-    let T1_Ans1 = "I\'ll hop in. Thanks for the help!"
-    let T1_Ans2 = "Better ask him if he\'s a murderer first."
+    // Our Strings
+    let story1 = "Your car has blown a tire on a winding road in the middle of nowhere with no cell phone reception. You decide to hitchhike. A rusty pickup truck rumbles to a stop next to you. A man with a wide brimmed hat with soulless eyes opens the passenger door for you and asks: \"Need a ride, boy?\"."
+    let answer1a = "I\'ll hop in. Thanks for the help!"
+    let answer1b = "Better ask him if he\'s a murderer first."
     
     
-    let T2_Story = "He nods slowly, unphased by the question."
-    let T2_Ans1 = "At least he\'s honest. I\'ll climb in."
-    let T2_Ans2 = "Wait, I know how to change a tire."
+    let story2 = "He nods slowly, unphased by the question."
+    let answer2a = "At least he\'s honest. I\'ll climb in."
+    let answer2b = "Wait, I know how to change a tire."
     
-    let T3_Story = "As you begin to drive, the stranger starts talking about his relationship with his mother. He gets angrier and angrier by the minute. He asks you to open the glovebox. Inside you find a bloody knife, two severed fingers, and a cassette tape of Elton John. He reaches for the glove box."
-    let T3_Ans1 = "I love Elton John! Hand him the cassette tape."
-    let T3_Ans2 = "It\'s him or me! You take the knife and stab him."
+    let story3 = "As you begin to drive, the stranger starts talking about his relationship with his mother. He gets angrier and angrier by the minute. He asks you to open the glovebox. Inside you find a bloody knife, two severed fingers, and a cassette tape of Elton John. He reaches for the glove box."
+    let answer3a = "I love Elton John! Hand him the cassette tape."
+    let answer3b = "It\'s him or me! You take the knife and stab him."
     
-    let T4_End = "What? Such a cop out! Did you know traffic accidents are the second leading cause of accidental death for most adult age groups?"
-    let T5_End = "As you smash through the guardrail and careen towards the jagged rocks below you reflect on the dubious wisdom of stabbing someone while they are driving a car you are in."
-    let T6_End = "You bond with the murderer while crooning verses of \"Can you feel the love tonight\". He drops you off at the next town. Before you go he asks you if you know any good places to dump bodies. You reply: \"Try the pier.\""
+    let story4 = "What? Such a cop out! Did you know traffic accidents are the second leading cause of accidental death for most adult age groups?"
+    let story5 = "As you smash through the guardrail and careen towards the jagged rocks below you reflect on the dubious wisdom of stabbing someone while they are driving a car you are in."
+    let story6 = "You bond with the murderer while crooning verses of \"Can you feel the love tonight\". He drops you off at the next town. Before you go he asks you if you know any good places to dump bodies. You reply: \"Try the pier.\""
     
     
     // UI Elements linked to the storyboard
@@ -31,7 +31,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var bottomButton: UIButton!      // Has TAG = 2
     @IBOutlet weak var storyTextView: UILabel!
     
-    // TODO Step 5: Initialise instance variables here
+    // TODO Step 5: Initialise variables here
     
     
     
@@ -40,13 +40,13 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         
-        // TODO Step 3: Set the text for the storyTextView, topButton, bottomButton, and to T1_Story, T1_Ans1, and T1_Ans2
+        // TODO Step 3: Set the text for the storyTextView, topButton, bottomButton
         
     }
 
     
-    // User presses one of the buttons
-    @IBAction func buttonPressed(sender: UIButton) {
+
+    @IBAction func buttonPressed(_ sender: UIButton) {
     
         // TODO Step 4: Write an IF-Statement to update the views
                 
@@ -56,10 +56,7 @@ class ViewController: UIViewController {
     }
     
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+
 
 
 }
